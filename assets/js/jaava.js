4c73a9a7a9ad9b976531fd331c5355104d3e2db8
Rashim <122736673+rashim2104@users.noreply.github.com>
2023-07-10 18:02:19 +0530

    1st
    Header,Logo,Slide

---
+++ assets/js/jaava.js
@@ -0,0 +1,8 @@
+function toggleSlide() {
+    var slideContainer = document.getElementById("slideContainer");
+    var moreImg = document.querySelector(".threel img");
+    
+    slideContainer.classList.toggle("active");
+    moreImg.classList.toggle("active");
+  }
+  
